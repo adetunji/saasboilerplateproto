d1cb85c2b072dbb71dabc0fe15945eec3691f934
adetunji <adecorelabs@gmail.com>
2020-10-09 15:53:59 -0400

    Worked on theming

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuesax, {
+  
+})
 
 new Vue({
   router,
